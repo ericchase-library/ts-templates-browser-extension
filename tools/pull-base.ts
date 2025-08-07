7a7cea41c85b65a740ee859ca6613bb61bc4c4bd
ericchase <github@eric.addymail.com>
2025-08-07 03:52:27 -0500

    up
    Signed-off-by: ericchase <github@eric.addymail.com>

--- tools/pull-base.ts
+++ tools/pull-base.ts
@@ -1,6 +1,6 @@
 import { Step_Dev_Format } from './core-dev/step/Step_Dev_Format.js';
-import { Step_Dev_Project_Sync_Config } from './core-dev/step/Step_Dev_Project_Sync_Config.js';
-import { Step_Dev_Project_Sync_Lib } from './core-dev/step/Step_Dev_Project_Sync_Lib.js';
+import { Step_Dev_Project_Sync_Core } from './core-dev/step/Step_Dev_Project_Sync_Core.js';
+import { Step_Dev_Project_Update_Config } from './core-dev/step/Step_Dev_Project_Update_Config.js';
 import { Builder } from './core/Builder.js';
 import { Step_Bun_Run } from './core/step/Step_Bun_Run.js';
 
@@ -9,8 +9,8 @@ import { Step_Bun_Run } from './core/step/Step_Bun_Run.js';
 
 Builder.SetStartUpSteps(
   Step_Bun_Run({ cmd: ['bun', 'install'], showlogs: false }),
-  Step_Dev_Project_Sync_Lib({ from_path: 'C:/Code/Base/JavaScript-TypeScript/@Template', to_path: './' }),
-  Step_Dev_Project_Sync_Config({ project_path: './' }),
+  Step_Dev_Project_Sync_Core({ from_path: 'C:/Code/Base/JavaScript-TypeScript/Templates/Browser-Extension', to_path: './' }),
+  Step_Dev_Project_Update_Config({ project_path: './' }),
   Step_Dev_Format({ showlogs: false }),
   //
 );
