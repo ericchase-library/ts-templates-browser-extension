9ab8f9e0f18bddca3f5f05fefe3e14abfd6bd50a
ericchase <github@eric.addymail.com>
2025-08-15 13:22:03 -0500

    up
    Signed-off-by: ericchase <github@eric.addymail.com>

--- tools/pull-lib.ts
+++ tools/pull-lib.ts
@@ -7,11 +7,12 @@ import { Step_FS_Mirror_Directory } from './core/step/Step_FS_Mirror_Directory.j
 // This script pulls template lib files from template project.
 
 const template_path = 'C:/Code/Base/JavaScript-TypeScript/Templates/Browser-Extension';
+const lib_folders: string[] = ['lib-browser-extension'];
 
 Builder.SetStartUpSteps(
   Step_Bun_Run({ cmd: ['bun', 'install'], showlogs: false }),
   Step_Async(
-    ['lib-browser-extension'].map((dir: string) =>
+    lib_folders.map((dir: string) =>
       Step_FS_Mirror_Directory({
         from_path: NODE_PATH.join(template_path, 'tools/' + dir),
         to_path: NODE_PATH.join(Builder.Dir.Tools, dir),
